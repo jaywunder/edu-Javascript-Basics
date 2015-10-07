accd280532ba2bb0c1cb93712cae667f769b0cc8
jaywunder <jaywunder@gmail.com>
2015-10-06 21:15:10 -0400

    probably going to have code example in the README doc, and each section in files

--- 00-data-types.js
+++ 00-data-types.js
@@ -13,6 +13,26 @@ console.log('foo: ' + foo); // uncomment this line to see the value of foo.
 console.log('bar: ' + bar); // DON'T USE VARIABLES BEFORE YOU SET THEM!!s
 var bar;
 
+var baz = 1;
+
+baz + 1; // this doesn't change baz
+
+// this will change the value of baz
+baz = baz + 1; // addition
+baz = baz - 1; // subtraction
+baz = baz * 1; // multiplication
+baz = baz / 1; // division
+
+console.log('baz: ' + baz);
+
+// it's cleaner to use these operators though
+baz += 1; // addition
+baz -= 1; // subtraction
+baz *= 1; // multiplication
+baz /= 1; // division
+
+console.log('baz: ' + baz);
+
 ///////////
 //NUMBERS//
 ///////////
@@ -31,8 +51,26 @@ console.log(20 / 3);
 ///////////
 //STRINGS//
 ///////////
+// a string contains something that you don't want to be interpreted as code
+var string = 'this is jsut text, it can have numbers too! 123i5y4327';
 
+string += ' | appended using `+=`';
+
+console.log(string);
 
 ////////////
 //BOOLEANS//
 ////////////
+// boolean means "either true or false"
+
+var yes = true;
+var no = false;
+
+if (yes) {
+  console.log('yay!');
+} else if (no) {
+  console.log('this will never be called');
+}
+
+// use booleans to store something that only has two possible states such as "on/off"
+// DON'T use booleans for something like color, where there's more than two colors
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-#Javascript Basics
+# Javascript Basics
+
+### Table of Contents  
+|||
+|:- | :--------- |
+| 1 | Primitives |
+| 2 | Arrays     |
+| 3 | Objects    |
+| 4 | Functions  |
+| 5 | Classes    |
+
+## Primitives
+
+### Boolean
+
+### Null
+
+### Undefined
+
+### Number
+
+### String
+
+## Arrays
+
+## Objects
+
+## Functions
+
+## Classes
